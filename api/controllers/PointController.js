4ed2fd3fb11650121dbe4f481c4f214cccb5bf49
Sebastián Lastra Mena <sebastian.lastra@usach.cl>
2015-05-28 22:48:00 -0300

    first commit

---
+++ api/controllers/PointController.js
@@ -0,0 +1,11 @@
+/**
+ * PointController
+ *
+ * @description :: Server-side logic for managing points
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Point.js
@@ -0,0 +1,22 @@
+/**
+* Point.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      latitude: 'string',
+      longitude: 'string',
+      accuracy: 'string',
+      altitude: 'string',
+      provider: 'string',
+      bearing: 'string',
+      speed: 'string',
+      bearing: 'string',
+      battery: 'string',
+      charging: 'string'
+  }
+};
